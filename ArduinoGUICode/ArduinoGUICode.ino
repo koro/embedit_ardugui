00da0825acff5f1345556f4f69f065b2773bc0aa
Heinrich Mellmann <mellmann@informatik.hu-berlin.de>
2019-01-09 18:53:08 +0100

    symmetric control added a simple filter to the sensor imput

--- ArduinoGUICode/ArduinoGUICode.ino
+++ ArduinoGUICode/ArduinoGUICode.ino
@@ -5,15 +5,23 @@
 Servo myServo1;
 Servo myServo2;
 
-// define pins
-byte servoPin1 = 3;
-byte servoPin2 = 5;
+/*
+// PINS (ALPHA)
+byte servoPin1 = 4;
+byte servoPin2 = 11;
 
-byte sensorPinVelocity1 = A0;
-byte sensorPinVelocity2 = A2;
+byte sensorPinVelocity = A0;
+byte sensorPinAmplitude = A3;
+byte sensorPinPhaseOffset = A6;
+*/
 
-byte sensorPinAmplitude1 = A1;
-byte sensorPinAmplitude2 = A3;
+// PINS (BETA)
+byte servoPin1 = 12;
+byte servoPin2 = 13;
+
+byte sensorPinVelocity = A0;
+byte sensorPinAmplitude = A3;
+byte sensorPinPhaseOffset = A5;
 
 //byte servoMin = 5;
 //byte servoMax = 170;
@@ -21,15 +29,16 @@ byte sensorPinAmplitude2 = A3;
 const int timestep = 10;
 const float timestepInS = ((float)timestep) / 1000.0;
 
-const float maxAngleSpeed = 360.0; // in deg
-const float maxAmplitude = 180.0; // in deg
+const float maxAngleSpeed = 720.0; // in deg
+
+const float maxAmplitude = 90.0; // in deg
 
 // values for the oscillators
-float pos1 = 0;
-float pos2 = 0;
+float pos = 0;
 
 void setup() 
 {
+  analogReference(DEFAULT);
   Serial.begin(9600);
   
   // set the start values for both servos to the middle
@@ -43,30 +52,49 @@ void setup()
 
 // returns a float value in [0,1]
 float readFloatValue(int port) {
-  return ((float)analogRead(port)) / 1023.;
+  float v = ((float)analogRead(port));
+  Serial.print(v);
+  Serial.print(" ");
+  return v / 1023.;
 }
+
+float amplitude = 0;
+float velocity = 0;
+float offset = 0;
 
 void loop()
 {
-  { // update the servo 1
-    float amplitude = 40.0;
-    float velocity = readFloatValue(sensorPinVelocity1) * maxAngleSpeed;
-    Serial.println(velocity);
-    
-    pos1 += velocity * timestepInS;
-    int p = (int)(90.0 + sin(pos1 / 180.0 * PI)*amplitude);
-    myServo1.write(p);
-  }
+  amplitude = 0.9*amplitude + 0.1*readFloatValue(sensorPinAmplitude) * maxAmplitude;
+  velocity = 0.9*velocity + 0.1*readFloatValue(sensorPinVelocity) * maxAngleSpeed;
+  offset = 0.9*offset + 0.1*readFloatValue(sensorPinPhaseOffset);
+
+  // constant drive test
+  /*
+  amplitude = maxAmplitude;
+  velocity = maxAngleSpeed;
+  offset = 0;
+  */
   
-  { // update the servo 2
-    float amplitude = 40.0;
-    float velocity = readFloatValue(sensorPinVelocity2) * maxAngleSpeed;
-    Serial.println(velocity);
-    
-    pos2 += velocity * timestepInS;
-    int p = (int)(90.0 + sin(pos2 / 180.0 * PI)*amplitude);
-    myServo2.write(p);
-  }
+  pos += velocity * timestepInS;
   
+  int p1 = (int)(90.0 + sin(pos / 180.0 * PI + offset*PI*0.5)*amplitude);
+  myServo1.write(p1);
+
+  //int p1 = (int)(1500.0 + sin(pos / 180.0 * PI + offset*PI*0.5)*amplitude);
+  //myServo1.writeMicroseconds(p1);
+  
+  int p2 = (int)(90.0 + sin(pos / 180.0 * PI - offset*PI*0.5)*amplitude);
+  myServo2.write(p2);
+
+  //int p2 = (int)(1500.0 + sin(pos / 180.0 * PI - offset*PI*0.5)*amplitude);
+  //myServo2.writeMicroseconds(p2);
+
+  /*
+  Serial.print(p1);
+  Serial.print(" ");
+  Serial.print(p2);
+  */
+
+  Serial.println();
   delay(timestep);
 }
